e5a29a34941fcfeafad161274c2d32f3c910fccd
rey-coyrehourcq sebastien <s.rey.coyrehourcq@gmail.com>
2017-01-06 17:43:40 +0100

    nested airport in country

---
+++ flight/flight_project/items/Items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class CountryItem(scrapy.Item):
+    name = scrapy.Field()
+    link = scrapy.Field()
+    airports = scrapy.Field()
+    other_url= scrapy.Field()
+    last_updated = scrapy.Field(serializer=str)
+
+class AirportItem(scrapy.Item):
+    name = scrapy.Field()
+    code_little = scrapy.Field()
+    code_total = scrapy.Field()
+    lat = scrapy.Field()
+    lon = scrapy.Field()
+    link = scrapy.Field()
--- flight/flight_project/spiders/AirportsSpider.py
+++ flight/flight_project/spiders/AirportsSpider.py
@@ -1,12 +1,19 @@
 import scrapy
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.spiders import Rule
-from flight_project.items.airportItems import AirportItem
+from flight_project.items.Items import *
+from bs4 import BeautifulSoup
+
+
 
 class AirportsSpider(scrapy.Spider):
     name = "airports"
     start_urls = ['https://www.flightradar24.com/data/airports']
     allowed_domains = ['flightradar24.com']
+
+    def clean_html(self, html_text):
+        soup = BeautifulSoup(html_text, 'html.parser')
+        return soup.get_text()
 
     rules = [
     # Extract links matching 'item.php' and parse them with the spider's method parse_item
@@ -14,7 +21,45 @@ class AirportsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        item = AirportItem()
-        item['name'] = response.xpath('//a[@data-country]/@title').extract()
-        item['link'] = response.xpath('//a[@data-country]/@href').extract()
+
+        self.logger.info("Visited main page %s", response.url)
+        for country in response.xpath('//a[@data-country]'):
+            item = CountryItem()
+            url =  country.xpath('./@href').extract()
+            name = country.xpath('./@title').extract()
+            item['link'] = url[0]
+            item['name'] = name[0]
+            self.logger.info("Airport name : %s with link %s" , item['name'] , item['link'])
+            yield scrapy.Request(url[0],meta={'my_country_item':item}, callback=self.parse_page2)
+
+
+        #return item
+
+    def parse_page2(self,response):
+        item = response.meta['my_country_item']
+        self.logger.info("Airport name : %s", item['name'])
+        self.logger.info("Visited airport page %s", response.url)
+        airports = []
+
+        for airport in response.xpath('//a[@data-iata]'):
+            url = airport.xpath('./@href').extract()
+            iata = airport.xpath('./@data-iata').extract()
+            iatabis = airport.xpath('./small/text()').extract()
+            name = ''.join(airport.xpath('./text()').extract()).strip()
+            lat = airport.xpath("./@data-lat").extract()
+            lon = airport.xpath("./@data-lon").extract()
+            self.logger.info("Name = %s", name)
+            iAirport = AirportItem()
+            iAirport['name'] = self.clean_html(name)
+            iAirport['link'] = url[0]
+            iAirport['lat'] = lat[0]
+            iAirport['lon'] = lon[0]
+            iAirport['code_little'] = iata[0]
+            iAirport['code_total'] = iatabis[0]
+
+            airports.append(iAirport)
+
+        item['airports'] = airports
+
         return item
+
